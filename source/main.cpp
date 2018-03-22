b025b5c8c941dd6e7889d2ca3a3b5a65e3f4701e
Mickael Grise-Roy <mickaelgriseroy@hotmail.com>
2018-03-22 10:17:41 -0400

    Création du projet.

---
+++ source/main.cpp
@@ -0,0 +1,10 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Quit();
+
+    return 0;
+}
