47db03610f0b904992d919fd9a78a57f961da2f4
Jade St-Pierre Bouchard <jade070@hotmail.com>
2018-03-24 18:06:30 -0400

    Modify - ProjetFinal - Ajout du constructeur / destructeur / fonction Run

--- source/ProjetFinal.h
+++ source/ProjetFinal.h
@@ -1,6 +1,6 @@
 /// \brief Représentation du cadre du jeu.
 /// \details Le coeur du projet, l'application.
-/// \author Antoine Legault
+/// \author Antoine Legault, Jade St-Pierre Bouchard
 /// \date 24 mars 2018
 /// \version 0.1
 /// \warning Mettre les warning si nécessaire.
@@ -8,8 +8,68 @@
 #ifndef SOURCE_PROJETFINAL_H
 #define SOURCE_PROJETFINAL_H
 
+#include <list>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
 
-class ProjetFinal {
+
+#include "Singleton.h"
+#include "ResourceManager.h"
+#include "GLContext.h"
+#include "Menu.h"
+#include "Observable.h"
+
+class ProjetFinal : public Singleton {
+public:
+    ResourceManager* ressourceManagerList; ///< Composante Liste de ressourceManager
+    GLContext* glContext; ///< Composante Liste de ressourceManager
+    std::list<Menu>* menuList; ///< Composante
+    //GLContext glContext("SDL - OpenGL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 700, 0);
+
+
+    ProjetFinal(const char* title, int x, int y, int width, int height, unsigned int windowflags = 0){
+        ressourceManagerList = new ResourceManager();
+        glContext = new GLContext(title,x ,y ,width , height, windowflags);
+        menuList = new std::list<Menu>;
+    }
+
+    ProjetFinal~(){
+        delete (glContext);
+        delete (menuList);
+        delete (ressourceManagerList);
+    }
+
+    void run(){
+
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_TEXTURE_2D);
+        glEnable(GL_LIGHTING);
+        glEnable(GL_LIGHT0);
+
+        SDL_ShowCursor(SDL_DISABLE);
+
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        SDL_Event sdlEvent;
+        /*
+         * déplacement de la souris
+        sdlEvent.motion.xrel;
+
+         */
+        bool isOpen = true;
+        while (isOpen){
+            while(SDL_PollEvent(&sdlEvent)) {
+                switch (sdlEvent.type) {
+                    case SDL_QUIT:
+                        isOpen = false;
+                        break;
+                }
+            }
+            glContext->clear();
+            glContext->refresh();
+    }
+
+
 
 };
 
--- source/main.cpp
+++ source/main.cpp
@@ -1,44 +1,18 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_image.h>
 
-#include "GLContext.h"
+#include "ProjetFinal.h"
 #include "Matrix.h"
+
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Init(SDL_INIT_VIDEO);
 
-    GLContext glContext("SDL - OpenGL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 700, 0);
+    ProjetFinal* projet = new ProjetFinal("SDL - OpenGL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 700, 0);
     //glContext.setFrustum(90.0, 0.1, 1000.0);
 
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_TEXTURE_2D);
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-
-    SDL_ShowCursor(SDL_DISABLE);
-
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    SDL_Event sdlEvent;
-    /*
-     * déplacement de la souris
-    sdlEvent.motion.xrel;
-
-     */
-    bool isOpen = true;
-    while (isOpen){
-        while(SDL_PollEvent(&sdlEvent)) {
-            switch (sdlEvent.type) {
-                case SDL_QUIT:
-                    isOpen = false;
-                    break;
-            }
-        }
-        glContext.clear();
-        glContext.refresh();
-    }
+    projet->run();
 
     SDL_Quit();
+
+    delete (projet);
 
     return 0;
 }
