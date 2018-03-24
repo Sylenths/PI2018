92e59ae75afd4f10d38667df39f48aa20d053e37
Antoine Legault <antoine199815@live.ca>
2018-03-24 17:24:54 -0400

    Modify - main - commencement de l'interface

--- source/main.cpp
+++ source/main.cpp
@@ -2,8 +2,42 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 
+#include "GLContext.h"
+#include "Matrix.h"
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO);
+
+    GLContext glContext("SDL - OpenGL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 700, 0);
+    //glContext.setFrustum(90.0, 0.1, 1000.0);
+
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+
+    SDL_ShowCursor(SDL_DISABLE);
+
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    SDL_Event sdlEvent;
+    /*
+     * déplacement de la souris
+    sdlEvent.motion.xrel;
+
+     */
+    bool isOpen = true;
+    while (isOpen){
+        while(SDL_PollEvent(&sdlEvent)) {
+            switch (sdlEvent.type) {
+                case SDL_QUIT:
+                    isOpen = false;
+                    break;
+            }
+        }
+        glContext.clear();
+        glContext.refresh();
+    }
+
     SDL_Quit();
 
     return 0;
