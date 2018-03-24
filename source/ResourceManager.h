5645955123688b5cebae534a45f9019925f9730c
Jade St-Pierre Bouchard <jade070@hotmail.com>
2018-03-24 19:12:37 -0400

    Modify - ResourceManager - Mise en place des méthodes addResource, removeResource et getResource

--- source/ResourceManager.h
+++ source/ResourceManager.h
@@ -1,9 +1,9 @@
 /// \brief Représentation de tous les ressources utilisées dans le projet.
 /// \details Interface permettant d'avoir acces, de rajouter ou d'enlever des textures ou des fonts.
-/// \author Antoine Legault
+/// \author Antoine Legault, Jade St-Pierre Bouchard
 /// \date 24 mars 2018
 /// \version 0.1
-/// \warning Mettre les warning si nécessaire.
+/// \warning Si vous utilisez la méthode add avec un nom qui est déjà utilisé cela écrasera l'ancienne resource.
 /// \bug Problèmes connus
 
 #ifndef SOURCE_RESOURCEMANAGER_H
@@ -17,15 +17,19 @@ private:
     std::map<std::string, Resource*> resources;  ///< Composante Map de textures.
 public:
     /// Ajouter une texture.
-    void addResource(){
-
+    void addResource(std::string resourceKey, Resource* resource){
+        resources[resourceKey] = resource;
     }
     /// Supprimer une texture.
-    void removeResource(){
+    void removeResource(std::string resourceKey){
+        if(resources[resourceKey])
+            resources.erase(resourceKey);
 
     }
     /// Récupérer une texture.
-    void getResource(){
+    Resource* getResource(std::string resourceKey){
+        if(resources[resourceKey])
+            return resources[resourceKey];
 
     }
 };
