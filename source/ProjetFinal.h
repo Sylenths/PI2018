c0179909e31be557e64e74123ade59e0229bd165
Gabriel Bourdages <gabriel_bourdages@outlook.com>
2018-03-27 14:00:49 -0400

    modify - ProjetFinal - Ajout de la méthode getID, ResourceManager est maintenant singleton

--- source/ProjetFinal.h
+++ source/ProjetFinal.h
@@ -22,7 +22,6 @@
 
 class ProjetFinal : public Singleton<ProjetFinal> {
 private:
-    ResourceManager* resourceManager; ///< Ressource Manager qui va nous permettre de gérer nos ressources.
     GLContext* glContext; ///< GlContext qui va s'occuper de la l'affichage.
     std::list<Menu>* menuList; ///< List de menu
     SDL_Event* sdlEvent;
@@ -34,7 +33,6 @@ private:
 public:
 
     ProjetFinal(const char* title = "P.I. 2018", int x = SDL_WINDOWPOS_CENTERED, int y = SDL_WINDOWPOS_CENTERED, int width = 1280, int height = 720, unsigned int windowflags = 0){
-        resourceManager = new ResourceManager();
         glContext = new GLContext(title,x ,y ,width , height, windowflags);
         menuList = new std::list<Menu>;
         glContext->setFrustum(90.0, 0.1, 1000.0, false);
@@ -45,10 +43,25 @@ public:
     ~ProjetFinal () {
         delete (glContext);
         delete (menuList);
-        delete (resourceManager);
         delete (sdlEvent);
     }
     /// Représente la boucle de jeu
+
+    void getID(const char* filename, std::string textureName){
+            unsigned int TextureID;
+            glGenTextures(1, &TextureID);
+            glBindTexture(GL_TEXTURE_2D, TextureID);
+
+            SDL_Surface* surface = IMG_Load(filename);
+            glTexImage2D(GL_TEXTURE_2D, 0 , GL_RGBA, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+            SDL_FreeSurface(surface);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+            ResourceManager::getInstance()->addTexture(textureName, TextureID);
+    }
+
     void run(){
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_TEXTURE_2D);
