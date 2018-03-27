c62c003bcf1a76cb0137567d367ae3839f73a925
Guillaume Julien-Desmarchais <36198554+GuillaumeJulienDesmarchais@users.noreply.github.com>
2018-03-27 14:17:31 -0400

    Modify - Font  Constructeur/Destructeur

--- source/Font.h
+++ source/Font.h
@@ -1,20 +1,31 @@
 /// \brief Gestionnaire de police d'écriture.
 /// \details Permet de récupérer la police d'écriture..
-/// \author Antoine Legault
+/// \authors Antoine Legault, Guillaume Julien-Desmarchais
 /// \date 24 mars 2018
-/// \version 0.1
+/// \version 0.4
 /// \warning Mettre les warning si nécessaire.
 /// \bug Problèmes connus
 #ifndef SOURCE_FONT_H
 #define SOURCE_FONT_H
 
+#include <SDL2/SDL_ttf.h>
 #include "Resource.h"
-
+#include "ResourceManager.h"
 class Font : public Resource{
-public:
 
 private:
+    TTF_Font* font;
+public:
 
+    Font(const char* filename, const char* RessourceName, int pixelSize = 12){ //
+        font = TTF_OpenFont(filename, pixelSize);
+        ResourceManager::getInstance()->addResource(RessourceName, this);
+    }
+
+    ~Font(){
+        TTF_CloseFont(font);
+        font = nullptr; //To be safe...
+    }
 };
 
 
