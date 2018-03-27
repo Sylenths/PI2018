04544c59ba3600d704dfb2f87cf036f733c6079e
Samuel Labelle <36251961+HexagonalProcessor@users.noreply.github.com>
2018-03-27 09:20:01 -0400

    Debug - ResourceManager.h - Destructor should now be working.

--- source/ResourceManager.h
+++ source/ResourceManager.h
@@ -1,10 +1,10 @@
-/// \brief Représentation de tous les ressources utilisées dans le projet.
-/// \details Interface permettant d'avoir acces, de rajouter ou d'enlever des textures ou des fonts.
-/// \author Antoine Legault, Jade St-Pierre Bouchard, Tai Chen Li
-/// \date 24 mars 2018
-/// \version 0.1
-/// \warning Si vous utilisez la méthode add avec un nom qui est déjà utilisé cela écrasera l'ancienne resource. Aussi, il y a une destruction des données chaque fois que la méthode removeResource() est appelée.
-/// \bug Problèmes connus, le destructeur de fin de jeu ne fonctionne pas en code
+/// \brief Gestionnaire de toutes les ressources utilisées dans le projet.
+/// \details Gère et permet d'avoir accès à, de rajouter ou d'enlever des images, des textures, des modèles 3D et des polices de caractères associés à un nom.
+/// \author Antoine Legault, Jade St-Pierre Bouchard, Tai Chen Li, Samuel Labelle
+/// \date 27 mars 2018
+/// \version 0.2
+/// \warning Ajouter avec add() une ressource dont le nom est déjà utilisé écrasera l'ancienne ressource et causera une fuite de mémoire. removeResource() détruit la ressource en plus de libérer son identificateur.
+/// \bug Aucun.
 
 #ifndef SOURCE_RESOURCEMANAGER_H
 #define SOURCE_RESOURCEMANAGER_H
@@ -15,35 +15,35 @@
 
 class ResourceManager {
 private:
-    std::map<std::string, Resource*> resources;  ///< Map de textures.
+	std::map<std::string, Resource*> resources;  ///< Map de textures.
 public:
 
-    /// Destruction de toutes les ressources vers la fin du jeu. Utilisation de la magie auto.
-    ~ResourceManager() { /*
-        typename std::map<std::string, Resource*>::iterator it = resources.begin();
-        for (int i = 0; i <resources.size() ; ++i) {
-            delete resources[(it)];
-        }
-        */
-        resources.clear();
-    }
+	/// Destruction de toutes les ressources vers la fin du jeu.
+	~ResourceManager() {
 
-    /// Ajouter une ressource.
-    void addResource(std::string resourceKey, Resource* resource){
-        resources[resourceKey] = resource;
-    }
-    /// Supprimer une ressource AVEC DESTRUCTEUR. ATTENTION!
-    void removeResource(std::string resourceKey){
-        if(resources[resourceKey]) {
-            delete resources[resourceKey];
-            resources.erase(resourceKey);
-        }
-    }
-    /// Récupérer une ressource.
-    Resource* getResource(std::string resourceKey){
-        if(resources[resourceKey])
-            return resources[resourceKey];
-    }
+		std::map<std::string, Resource*>::iterator it = resources.begin();
+		while(it != resources.end()){
+			delete ((*it).second);// (*it).second is the Resource* of the current map "container"
+			it = resources.erase(it);// Remove map "container" at current it position then set it to next map "container".
+		}
+	}
+
+	/// Ajouter une ressource.
+	void addResource(std::string resourceKey, Resource* resource){
+		resources[resourceKey] = resource;
+	}
+	/// Supprimer une ressource AVEC DESTRUCTEUR. ATTENTION!
+	void removeResource(std::string resourceKey){
+		if(resources[resourceKey]) {
+			delete resources[resourceKey];
+			resources.erase(resourceKey);
+		}
+	}
+	/// Récupérer une ressource.
+	Resource* getResource(std::string resourceKey){
+		if(resources[resourceKey])
+			return resources[resourceKey];
+	}
 };
 
 
