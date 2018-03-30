f81108c168cd8975863f73f1f648f51304b92ab8
Patrick Singcaster <Patrick.Singcaster@cegep-lanaudiere.qc.ca>
2018-03-30 00:39:49 -0400

    Add - main.cpp - Ajout d'un exemple de directives de précompilations permettant de gérer différent système d'exploitation.

--- source/main.cpp
+++ source/main.cpp
@@ -3,26 +3,21 @@
 int main(int argc, char* argv[]) {
     std::string filePath = argv[0];
 
-    /* Échoue sous *NIX / Mac OSX et fait planter (SIGSEGV) le programme.
-     * (les systèmes unix-like (Linux, Mac OSX, ...) utilisent "/" pour séparer les dossiers ...)
-     * il faudrait détecter l'OS ou utiliser des #define pour compiler du code spécifique pour chaque OS pour que ça fonctionne.
-
-    while (filePath.back() != '\\') { // On doit écrire backslash deux fois de suite, car c'est un caractère spécial pour C et C++.
+    #ifdef _WIN32
+    while (filePath.back() != '\\') {
         filePath.pop_back();
     }
-     */
-
-
-  /*  while (filePath.back() != '/') { // On doit écrire backslash deux fois de suite, car c'est un caractère spécial pour C et C++.
+    #else
+    while (filePath.back() != '/') {
         filePath.pop_back();
-    }*/
+    }
+    #endif
 
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
     ResourceManager::getInstance()->addResource("font - arial12",new Font("../Fonts/arial.ttf"));
 
     ProjetFinal::getInstance()->run(filePath.c_str());
-
 
     ProjetFinal::deleteInstance();
     ResourceManager::deleteInstance();
