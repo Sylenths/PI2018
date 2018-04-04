b105f75a85ee087ed688be92b61d1ae0ee4899f4
Patrick Singcaster <Patrick.Singcaster@cegep-lanaudiere.qc.ca>
2018-04-04 13:23:45 -0400

    Modify - Button.h - Clean up du code.

--- source/Button.h
+++ source/Button.h
@@ -1,5 +1,5 @@
-/// \brief Représentation d'un bouton
-/// \details Pour réagir au clic de souris et autre si possible
+/// \brief Représentation d'un bouton.
+/// \details Bouton réagissant au clic de souris.
 /// \author Tai Chen Li, Samuel Labelle
 /// \date 28 mars 2018
 /// \version 0.1
@@ -13,35 +13,27 @@
 
 class Button : public Image2D {
 public:
-  std::function<void()> onClick;  ///< Le pointeur de méthode vers quoi on veut réagir avec nos événements
+  std::function<void()> onClick;  ///< Pointeur de méthode réagissant à un click de souris.
 
   /// Constructeur.
-	/// \param onClick Fonction Activée par le bouton.
-    /// \param textureID Identificateur de la texture.
-	/// \param x Position en x par rapport au coin gauche en haut.
-	/// \param y Position en y par rapport au coin gauche en haut.
+  /// \param onClick Fonction activée par un click de souris.
+  /// \param textureID Identificateur de la texture.
+  /// \param x Position en x par rapport au coin supérieur gauche de la fenêtre.
+  /// \param y Position en y par rapport au coin supérieur gauche de la fenêtre.
 	/// \param width Largeur du bouton.
 	/// \param height Hauteur du bouton.
-    Button( unsigned int textureID, unsigned int x, unsigned int y, unsigned int z, double width, double height) : Image2D(textureID,x,y,z,width,height){
-      onClick = nullptr;
-    }
-	/// Réception de notifications d'événements SDL.
+  Button( unsigned int textureID, unsigned int x, unsigned int y, unsigned int z, double width, double height) : Image2D(textureID, x, y, z, width, height) {
+    onClick = nullptr;
+  }
+
+  /// Réception de notification d'événement SDL.
 	/// \param sdlEvent Événement SDL.
-    void notify(SDL_Event* sdlEvent) {
-        if (sdlEvent->button.button == SDL_BUTTON_LEFT) {
-            if (sdlEvent->button.x >= x) {
-                if (sdlEvent->button.x <= (x + width)) {
-                    if (sdlEvent->button.y >= y) {
-                        if (sdlEvent->button.y <= (y + height)) {
-                            if (onClick) onClick();
-                        }
-                    }
-                }
-            }
-        }
-    }
-
+  void notify(SDL_Event* sdlEvent) {
+    if (sdlEvent->button.button == SDL_BUTTON_LEFT)
+      if ((sdlEvent->button.x >= x) && (sdlEvent->button.x <= (x + width)) && (sdlEvent->button.y >= y) && (sdlEvent->button.y <= (y + height)))
+        if (onClick)
+          onClick();
+  }
 };
-
 
 #endif
