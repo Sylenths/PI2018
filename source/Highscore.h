f64f7b0e9848908b765c51ce39e1da76afb9d38b
Jade St-Pierre Bouchard <jade070@hotmail.com>
2018-04-04 22:39:04 -0400

    Add - Scores - Permet de gérer les scores dans le menu highscores - Modify - Highscore - ajout du bouton back, des données membres

--- source/Highscore.h
+++ source/Highscore.h
@@ -10,25 +10,49 @@
 
 #include "Menu.h"
 #include "includes.h"
+#include "Scores.h"
+#include <iostream>
+#include <fstream>
+#include <cstdio>
 
 class Highscore : public Menu{
 private:
     Button* backButton;
+    Scores* scores[11];
+    std::string fichierSauvegardeScores;
+
+
 public:
 
 
     Highscore(){
-        backButton = new Button (498, 550, 0.l, 284, 113, ResourceManager::getInstance()->getTexture("backButton"));
+       // FILE * fichier;
+        //fichierSauvegardeScores = "../../fichierScores.txt";
+
+        backButton = new Button (498, 550, 0.l, 284, 113, ResourceManager::getInstance()->getTexture("backButton"),ResourceManager::getInstance()->getTexture("BackButtonOver"));
+        ResourceManager::getInstance()->addResource("backButton", backButton);
         backButton->onClick = [this]() {Scene::activeScene  = "MainMenu";};
+
+
+
+
     }
 
 
     void subscribeAll(std::map<unsigned int, Observable<SDL_Event*>*>* observables) {
+
+        if(!(*observables)[SDL_MOUSEBUTTONDOWN])
+            (*observables)[SDL_MOUSEBUTTONDOWN] = new Observable<SDL_Event*>;
+        if(!(*observables)[SDL_MOUSEMOTION])
+            (*observables)[SDL_MOUSEMOTION]= new Observable<SDL_Event*>;
+
         (*observables)[SDL_MOUSEBUTTONDOWN]->subscribe(ResourceManager::getInstance()->getResource<Button*>("backButton"));
+        (*observables)[SDL_MOUSEMOTION]->unsubscribe(ResourceManager::getInstance()->getResource<Button*>("backButton"));
     }
 
     void unsubscribeAll(std::map<unsigned int, Observable<SDL_Event*>*>* observables) {
         (*observables)[SDL_MOUSEBUTTONDOWN]->unsubscribe(ResourceManager::getInstance()->getResource<Button*>("backButton"));
+        (*observables)[SDL_MOUSEMOTION]->subscribe(ResourceManager::getInstance()->getResource<Button*>("backButton"));
     }
 
     void draw(){
