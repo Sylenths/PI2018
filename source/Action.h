3c1152c9b42a4af0473d175f6d1d969ec36894b9
Mickael Grise-Roy <mickaelgriseroy@hotmail.com>
2018-04-10 08:12:38 -0400

    ADD - ajout de la classe action

---
+++ source/Action.h
@@ -0,0 +1,16 @@
+
+/// \brief Classe d'actions pouvant etre faites dans l'HUD
+/// \details Classe abstraite qui sera parents de tout les types d'actions pouvant être fait par le joueur
+/// \author Mickaël Grisé-Roy
+/// \date 10 Avril 2018
+/// \version 0.1
+/// \warning Aucun.
+/// \bug Aucun.
+#ifndef SOURCE_ACTION_H
+#define SOURCE_ACTION_H
+
+class Action {
+
+};
+
+#endif //SOURCE_ACTION_H
