7d952644031964aff250a1114c0694bfd3c350a9
Mickael Grise-Roy <mickaelgriseroy@hotmail.com>
2018-04-10 08:00:09 -0400

    conflit

--- source/Highscore.h
+++ source/Highscore.h
@@ -81,7 +81,8 @@ public:
             std::string labelbuffer;
             std::string labelNameBuffer;
             char intCharBuffer[10];
-            itoa(scores[j]->getScore(),intCharBuffer,10);
+            //TODO : fixer itoa pour linux
+            //itoa(scores[j]->getScore(),intCharBuffer,10);
             labelbuffer = scores[j]->getName()+ "    " + intCharBuffer;
             // Créer mes labels.
             switch (j){
