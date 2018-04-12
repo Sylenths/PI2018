53e28c47527c850a52f3eb015ef68c3e4d92a038
Patrick Singcaster <singcaster@protonmail.com>
2018-04-12 10:12:18 -0400

    Modify - ActionParameter.h - Généralisation de la classe.

--- source/ActionParameter.h
+++ source/ActionParameter.h
@@ -1,19 +1,16 @@
-//
-// Created by etudiant on 18-04-10.
-//
-
 #ifndef SOURCE_SIMPARAMETER_H
 #define SOURCE_SIMPARAMETER_H
-class Actionparameter{
+
+class ActionParameter{
 private:
     std::string name;
+
 public:
-    Actionparameter(std::string name){
+    ActionParameter(std::string name){
         this->name = name;
     }
-    template <typename T>
-    virtual T getData() = 0;
+
+    virtual std::string getType() = 0;
 };
 
-
-#endif //SOURCE_SIMPARAMETER_H
+#endif
