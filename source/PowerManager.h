ddc7ea1204433b9aa59ee6016c838404961e6e04
Gabriel Bourdages <gabriel_bourdages@outlook.com>
2018-04-25 14:57:55 -0400

    Modif - PowerManager - créatoin de la matrice dadjacence

--- source/PowerManager.h
+++ source/PowerManager.h
@@ -2,23 +2,48 @@
 #define POWERMANAGER_H
 #include "includes.h"
 #include "PowerWire.h"
+#include "PowerAppareil.h"
+#include "PowerSource.h"
 
 class PowerManager : public Singleton<PowerManager> {
 private:
     std::map<std::pair<int, int>, PowerWire*> adjMatrice;
+    std::map<int, PowerAppareil*> mapAppareil;
+    std::map<int, PowerSource*> mapSource;
+    int appareilNbr, sourceNbr;
 public:
-    PowerManager();
-
-    void addSource() {
-
+    PowerManager() {
+        appareilNbr = 0;
+        sourceNbr = 0;
     }
 
-    void addAppareil() {
-
+    void addSource(PowerSource* source) {
+        mapSource[sourceNbr] = source;
+        for(int i = sourceNbr; i <= appareilNbr; ++i) {
+            std::pair<int, int> key1 = std::make_pair(sourceNbr, i);
+            adjMatrice[key1] = nullptr;
+            std::pair<int, int> key2 = std::make_pair(i, sourceNbr);
+            adjMatrice[key2] = nullptr;
+        }
+        sourceNbr--;
     }
 
-    void resetPower() {
+    void addAppareil(PowerAppareil* appareil) {
+        appareilNbr++;
+        mapAppareil[appareilNbr] = appareil;
+        for(int i = appareilNbr; i >= sourceNbr; --i) {
+            std::pair<int, int> key1 = std::make_pair(appareilNbr, i);
+            adjMatrice[key1] = nullptr;
+            std::pair<int, int> key2 = std::make_pair(i, appareilNbr);
+            adjMatrice[key2] = nullptr;
+        }
+    }
 
+    void addWire(int node1, int node2, PowerWire* wire) {
+        std::pair<int, int> key1 = std::make_pair(node1, node2);
+        std::pair<int, int> key2 = std::make_pair(node2, node1);
+        adjMatrice[key1] = wire;
+        adjMatrice[key2] = wire;
     }
 
     void updatePower() {
