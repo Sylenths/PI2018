056045b82b6113279496bfd8ec9c1e581c55ae19
Gabriel Bourdages <gabriel_bourdages@outlook.com>
2018-05-03 12:08:16 -0400

    test - PowerManager

--- source/PowerAppareil.h
+++ source/PowerAppareil.h
@@ -8,25 +8,54 @@ protected:
     int proximityIndice;
     double usedCurrent;
     double currentLeft;
+    bool feeded;
+    std::map<int, std::stack<int>> pathsMap;
+    int mapSize;
 public:
     PowerAppareil(double usedCurrent) {
         this->usedCurrent = usedCurrent;
         usedCurrent = currentLeft;
+        mapSize = 0;
+        feeded = false;
+    }
+
+    void pushBackPathsMap(int source, int keyPath) {
+        pathsMap[source].push(keyPath);
+
+    }
+
+
+
+    void clearPathsMap() {
+        pathsMap.clear();
+
     }
 
     void setProximite() {
         proximityIndice = 0;
-        for(std::map<int, std::stack<int>>::iterator it = pathsMap.begin(); it != pathsMap.end(); it++) {
-            proximityIndice += (*it).second.size();
-        }
+        //for(std::map<int, std::stack<int>>::iterator it = pathsMap.begin(); it != pathsMap.end(); it++) {
+        //    proximityIndice += (*it).second.size();
+        //}
     }
 
     int getProximite() {
         return proximityIndice;
     }
 
-    double getCurrentLeft() {
-        return currentLeft;
+    bool isFeeded() {
+        return feeded;
+    }
+
+    double updateCurrent(double currentIn) {
+        currentLeft -= currentIn;
+        if(currentLeft < 0.00) {
+            feeded = true;
+            return currentLeft * -1.0;
+        }
+        else {
+            feeded = false;
+            return 0.0;
+        }
     }
 };
 
